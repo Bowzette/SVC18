455e4079227c065219e317eb7bdf38b3dec6faad
Bowzette <mischa3294@gmail.com>
2018-10-19 14:44:50 +0200

    fixed & finished coordinate system

--- SVC18/HexField.cpp
+++ SVC18/HexField.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <math.h>
 #include <Windows.h>
 
 using namespace std;
@@ -19,19 +20,23 @@ using namespace std;
 	DESERT		--------
 
 	SoC Rulebook: catan(dot)com/down/?/files/downloads/catan_5th_ed_rules_eng_150303.pdf
+	example codes:
+	gamedevelopment.tutsplus(dot)com/tutorials/introduction-to-axial-coordinates-for-hexagonal-tile-based-games--cms-28820
+	cplusplus(dot)com/forum/general/833/
 */
 class HexTile {
 	public:
 		int x, y, z;
 		enum fieldType { HILL, FOREST, MOUNTAIN, FIELD, PASTURE, DESERT };
+		fieldType fieldtype;
 		bool isRobber;
-		int numberToken = 1 + rand() % ((12 + 1) - 1); //platzhalter, da der Zahlenwert nicht wirklich zufällig ist. (die 7 gibts nicht)
+		int numberToken;
 		HexTile() {}
 		HexTile(int x, int y, fieldType fieldType) {
 			this->x = x;
-			this->y = y;
-			z = -x - y; //explanation gamedevelopment.tutsplus(dot)com/tutorials/introduction-to-axial-coordinates-for-hexagonal-tile-based-games--cms-28820
-			//this->fieldType = fieldType;
+			this->y = (y-(floor(x/2))); //double to int may be a problem???
+			z = -x - this->y; //TODO: improve code
+			this->fieldtype = fieldType;
 		}
 		string getInfo() {
 			return to_string(x) + "," + to_string(y) + "," + to_string(z);
@@ -40,21 +45,20 @@ class HexTile {
 
 class HexField {
 	public:
-		HexField() { //create standard game field
-
-		}
+		HexField() {} //create standard game field here not in main()
 };
 
 int main() {
 	srand(time(0));
-	cout << "map generator test" << endl;
-	vector< vector<HexTile> > field(5, vector<HexTile>(5)); //explanation cplusplus(dot)com/forum/general/833/
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++) {
+	cout << "map generator test" << endl << endl << endl;
+	int TESTSIZE = 5;
+	vector< vector<HexTile> > field(TESTSIZE, vector<HexTile>(TESTSIZE));
+	for (int i = 0; i < TESTSIZE; i++) {
+		for (int j = 0; j < TESTSIZE; j++) {
 			field[i][j] = HexTile(i, j, HexTile::HILL);
 			cout << field[i][j].getInfo() << "\t\t";
 		}
-		cout << endl;
+		cout << endl << endl << endl;
 	}
 
 	Sleep(8000);
